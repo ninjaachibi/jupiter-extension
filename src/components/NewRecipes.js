9d427ee3e53b364187584800dad295bbc90ac98c
Robert L <1998.robert@gmail.com>
2020-08-21 03:39:26 -0500

    [new recipes] add autocomplete text input field

--- src/components/NewRecipes.js
+++ src/components/NewRecipes.js
@@ -4,7 +4,7 @@ import React from "react";
 import TextField from '@material-ui/core/TextField';
 import SectionHeader from "./typo/SectionHeader";
 import CircularProgress from '@material-ui/core/CircularProgress';
-
+import { Autocomplete } from '@material-ui/lab';
 
 const query = `
 query searchProduct {
@@ -23,27 +23,91 @@ const opts = {
 };
 
 async function searchProducts(query) {
-    await fetch(url, opts)
-        .then(res => res.json())
-        .then(console.log)
-        .catch(console.error);
+    try {
+        const response = await fetch(url, opts)
+            .then(res => res.json())
+        // console.log(response)
+        return response;
+    }
+    catch (e) {
+        console.error(e);
+    }
+
 }
 
 export default function NewRecipes() {
     const [queryString, setQueryString] = React.useState(""); // TODO: add typescript
     const [ingredientList, setIngredientList] = React.useState([]); // Array<Ingredient> // TODO: add typescript for an Ingredient {id, name, ct}
-    
+
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = React.useState([]);
+    const loading = open && options.length === 0;
+
     React.useEffect(() => {
-        searchProducts(query);
-    })
+        let active = true;
 
+        if (!loading) {
+            return undefined;
+        }
 
-    return (
-    <SectionHeader
-        title="New recipe"
-        subtitle="Create a new recipe"
-    />
+        (async () => {
+            const response = await searchProducts(query);
+            console.log(response.data.search)
+
+            if (active) {
+                setOptions(response.data.search)
+            }
+        })();
+
+        return () => {
+            active = false;
+        };
+    }, [loading]);
+
+    React.useEffect(() => {
+        if (!open) {
+            setOptions([]);
+        }
+    }, [open]);
     // TODO: add save, cancel buttons
+
+    return (<>
+        <SectionHeader
+            title="New recipe"
+            subtitle="Create a new recipe"
+        />
+        <Autocomplete
+            id="asynchronous-demo"
+            style={{ width: 300 }}
+            open={open}
+            onOpen={() => {
+                setOpen(true);
+            }}
+            onClose={() => {
+                setOpen(false);
+            }}
+            getOptionSelected={(option, value) => option.name === value.name}
+            getOptionLabel={(option) => option.name}
+            options={options}
+            loading={loading}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Search for ingredients"
+                    variant="outlined"
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                    }}
+                />
+            )}
+        />
+    </>
     );
 }
 
