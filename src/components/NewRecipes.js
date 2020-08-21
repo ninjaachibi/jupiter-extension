e602d895d4971c216c73dbff3a983ce07a7bb11b
Robert L <1998.robert@gmail.com>
2020-08-21 03:09:22 -0500

    [new recipes] things work

--- src/components/NewRecipes.js
+++ src/components/NewRecipes.js
@@ -2,10 +2,8 @@
 import React from "react";
 
 import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
 import SectionHeader from "./typo/SectionHeader";
 import CircularProgress from '@material-ui/core/CircularProgress';
-
 
 
 const query = `
