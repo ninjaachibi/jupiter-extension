b54c29f1332c7ecd826cdd15a38105b1f11bbfc6
Robert L <1998.robert@gmail.com>
2020-08-21 17:59:29 -0500

    add login,signup + routing

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, HashRouter, Switch } from 'react-router-dom'
+import { Route, HashRouter, Switch, Redirect } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Wizard from './components/Wizard'
 import Cards from './components/Cards'
@@ -7,18 +7,50 @@ import Main from './components/Main'
 import Signup from './components/Signup'
 import ScrollToTop from './components/ScrollTop'
 import Recipes from './components/Recipes'
+import Login from './components/Login'
 
-export default props => (
+const user = null;
+
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user ? (
+          children
+        ) : (
+            <Redirect
+              to={{
+                pathname: "/signup",
+                state: { from: location }
+              }}
+            />
+          )
+      }
+    />
+  );
+}
+
+export default function (props) {
+
+  return (
     <HashRouter>
       <ScrollToTop>
         <Switch>
-          <Route exact path='/' component={ Main } />
-          <Route exact path='/recipes' component={ Recipes } />
-          <Route exact path='/dashboard' component={ Dashboard } />
-          <Route exact path='/signup' component={ Signup } />
-          <Route exact path='/wizard' component={ Wizard } />
-          <Route exact path='/cards' component={ Cards } />
+          <Route exact path='/' component={Main} />
+          <PrivateRoute exact path='/recipes' >
+            <Recipes />
+          </PrivateRoute>
+          <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/wizard' component={Wizard} />
+          <Route exact path='/cards' component={Cards} />
+          <Route exact path='/login'>
+            <Login />
+          </Route>
         </Switch>
       </ScrollToTop>
     </HashRouter>
-  )
+  );
+}
